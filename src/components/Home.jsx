e6e5355b44a9f0236f78363d8dac01df97586deb
Darlington <obidarlington09@gmail.com>
2024-10-06 22:21:47 +0100

    I think i fixed it, let's check

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -131,51 +131,51 @@ export const Home = () => {
   const Assest = ({ crypto }) => {
     return (
       <div className="assets">
-        <div className="assets-div">
-          <div className="assets-child">
+        <div className="assets-child">
+          <div className="asset-name">
             <div className="asset-img">
               <img src={crypto?.image?.large} alt="" />
             </div>
-            <div className="asset-name">
+            <div className="name-symbol">
               <h4>{crypto?.symbol}</h4>
               <p>{crypto?.name}</p>
             </div>
+          </div>
 
-            <div className="asset-graph">
-              <AssetGraph />
-            </div>
-            <div className="asset-price">
-              <h4>{`$${formatNumberWithCommas(
-                crypto?.market_data?.current_price?.usd
-              )}`}</h4>
-              {/* <p>{+$180 (0.9)%}</p> */}
-              <p
-                style={{
-                  color:
-                    crypto?.market_data?.price_change_24h_in_currency?.usd > 0
-                      ? "green"
-                      : "red",
-                }}
-              >
-                {Math.round(
-                  crypto?.market_data?.price_change_24h_in_currency?.usd * 10
-                ) /
-                  10 >
-                0
-                  ? `+$${formatNumberWithCommas(
-                      Math.round(
-                        crypto?.market_data?.price_change_24h_in_currency?.usd *
-                          10
-                      ) / 10
-                    )} `
-                  : `$${formatNumberWithCommas(
-                      Math.round(
-                        crypto?.market_data?.price_change_24h_in_currency?.usd *
-                          10
-                      ) / 10
-                    )}`}
-              </p>
-            </div>
+          <div className="asset-graph">
+            <AssetGraph />
+          </div>
+          <div className="asset-price">
+            <h4>{`$${formatNumberWithCommas(
+              crypto?.market_data?.current_price?.usd
+            )}`}</h4>
+            {/* <p>{+$180 (0.9)%}</p> */}
+            <p
+              style={{
+                color:
+                  crypto?.market_data?.price_change_24h_in_currency?.usd > 0
+                    ? "green"
+                    : "red",
+              }}
+            >
+              {Math.round(
+                crypto?.market_data?.price_change_24h_in_currency?.usd * 10
+              ) /
+                10 >
+              0
+                ? `+$${formatNumberWithCommas(
+                    Math.round(
+                      crypto?.market_data?.price_change_24h_in_currency?.usd *
+                        10
+                    ) / 10
+                  )} `
+                : `$${formatNumberWithCommas(
+                    Math.round(
+                      crypto?.market_data?.price_change_24h_in_currency?.usd *
+                        10
+                    ) / 10
+                  )}`}
+            </p>
           </div>
         </div>
       </div>
