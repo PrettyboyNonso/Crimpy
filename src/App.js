742c0322f3e19743c0aec77374723ddb97e4b2c3
Darlington <obidarlington09@gmail.com>
2024-10-08 21:56:03 +0100

    UI update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Home } from "./components/Home";
+import { Market } from "./components/Market";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Market />
     </div>
   );
 }
---
+++ src/components/Market.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer";
+
+export const Market = () => {
+  return (
+    <>
+      <div className="market">
+        <p>hello</p>
+      </div>
+      <Footer />
+    </>
+  );
+};
