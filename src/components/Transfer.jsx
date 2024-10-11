68ad128fea2ca411fd1fc9555fb8fa2aeba8bdd1
Darlington <obidarlington09@gmail.com>
2024-10-11 18:48:31 +0100

    transfer part done

--- src/components/Transfer.jsx
+++ src/components/Transfer.jsx
@@ -26,6 +26,46 @@ export const Transfer = () => {
           available balance : <b>$350,000</b>
         </p>
       </div>
+      <div className="number-btns">
+        <div className="btns">
+          <h4>1</h4>
+        </div>
+        <div className="btns">
+          <h4>2</h4>
+        </div>
+        <div className="btns">
+          <h4>3</h4>
+        </div>
+        <div className="btns">
+          <h4>4</h4>
+        </div>
+        <div className="btns">
+          <h4>5</h4>
+        </div>
+        <div className="btns">
+          <h4>6</h4>
+        </div>
+        <div className="btns">
+          <h4>7</h4>
+        </div>
+        <div className="btns">
+          <h4>8</h4>
+        </div>
+        <div className="btns">
+          <h4>9</h4>
+        </div>
+        <div className="btns">
+          <h4>0</h4>
+        </div>
+        <div className="btns">
+          <h4>00</h4>
+        </div>
+        <div className="btns"></div>
+      </div>
+
+      <div className="withdraw-btn">
+        <button>withdraw</button>
+      </div>
     </div>
   );
 };
